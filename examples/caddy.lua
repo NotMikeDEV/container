4b2bc03a872f845a0784bdcdfb3e21a1dbdf9419
Mike Jones <notmike@notmike.co.uk>
2016-09-21 21:18:42 +0100

    Updated caddy example to make it extendable

--- examples/caddy.lua
+++ examples/caddy.lua
@@ -1,32 +1,30 @@
 #!/usr/sbin/container
 
+function install_cgi()
+end
+
 function install_container()
 	exec("mkdir -p /usr/src/caddy")
 	install_package("ca-certificates")
 	exec("wget -O /usr/src/caddy/caddy.tar.gz \"https://caddyserver.com/download/build?os=linux&arch=amd64&features=\"")
 	exec("cd /usr/src/caddy; tar -zxf caddy.tar.gz")
 	exec("cp /usr/src/caddy/caddy /usr/bin")
+	install_cgi()
+end
+
+function run_cgi()
 end
 
 function run()
-	os.execute("/usr/bin/caddy -agree -email fake@user.com -conf /etc/Caddyfile -root /var/www")
+	run_cgi()
+	exec("/usr/bin/caddy -agree -email fake@user.com -conf /etc/Caddyfile -root /var/www")
 	return 0
 end
-
-request_IP("10.0.0.42", {nat=true})
---request_IP("66.85.79.6", {proxyarp='eth0'})
-
-request_IP("fd00::3", {nat=true})
---request_IP("2001:470:3922::1:42")
 
 filesystem['/var/www/'] = { type="map", path="docroot" }
 filesystem['/root/'] = { type="map", path="home" }
 config_files['/etc/Caddyfile'] = [[
-:80 {
-}
-
-https://caddy.notmike.uk {
-}
+:80
 ]]
 
 config_files['/var/www/index.html'] = [[
