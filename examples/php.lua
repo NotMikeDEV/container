1a2d0681c3cf8c590a0b54b8514553c342666008
Mike Jones <notmike@notmike.co.uk>
2016-09-21 21:20:32 +0100

    Update php example to import from caddy example

--- examples/php.lua
+++ examples/php.lua
@@ -1,25 +1,17 @@
 #!/usr/sbin/container
+require("caddy")
 
-function install_container()
-	exec("mkdir -p /usr/src/caddy")
-	install_package("ca-certificates")
-	exec("wget -O /usr/src/caddy/caddy.tar.gz \"https://caddyserver.com/download/build?os=linux&arch=amd64&features=\"")
-	exec("cd /usr/src/caddy; tar -zxf caddy.tar.gz")
-	exec("cp /usr/src/caddy/caddy /usr/bin")
+function install_cgi()
 	install_package("php5-fpm")
 end
 
-function run()
+function run_cgi()
 	exec("/usr/sbin/php5-fpm")
-	exec("/usr/bin/caddy -agree -email fake@user.com -conf /etc/Caddyfile -root /var/www")
-	return 0
 end
 
-request_IP("10.0.0.43", {nat=true})
---request_IP("66.85.79.6", {proxyarp='eth0'})
+request_IP("66.85.79.6", {proxyarp='eth0'})
 
-request_IP("fd00::43", {nat=true})
---request_IP("2001:470:3922::1:42")
+request_IP("2001:470:3922::1:42")
 
 filesystem['/var/log/'] = { type="map", path="log" }
 filesystem['/var/lib/php5/sessions'] = { type="tmpfs", size="512M" }
@@ -30,7 +22,7 @@ config_files['/etc/Caddyfile'] = [[
 	fastcgi / /var/run/php5-fpm.sock php
 }
 
-https://caddy.notmike.uk {
+https://c.notmike.uk {
 	fastcgi / /var/run/php5-fpm.sock php
 }
 ]]
