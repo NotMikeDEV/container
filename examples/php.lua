7d4c9d1053fa826708e71b9068729c3e83fbb81a
notmike-uk <notmike@notmike.co.uk>
2016-09-21 21:32:34 +0100

    Make php example extendable

--- examples/php.lua
+++ examples/php.lua
@@ -1,17 +1,17 @@
 #!/usr/sbin/container
 require("caddy")
 
+function install_site()
+end
+
 function install_cgi()
 	install_package("php5-fpm")
+	install_site()
 end
 
 function run_cgi()
 	exec("/usr/sbin/php5-fpm")
 end
-
-request_IP("66.85.79.6", {proxyarp='eth0'})
-
-request_IP("2001:470:3922::1:42")
 
 filesystem['/var/log/'] = { type="map", path="log" }
 filesystem['/var/lib/php5/sessions'] = { type="tmpfs", size="512M" }
@@ -19,10 +19,6 @@ filesystem['/var/www/'] = { type="map", path="docroot" }
 filesystem['/root/'] = { type="map", path="home" }
 config_files['/etc/Caddyfile'] = [[
 :80 {
-	fastcgi / /var/run/php5-fpm.sock php
-}
-
-https://c.notmike.uk {
 	fastcgi / /var/run/php5-fpm.sock php
 }
 ]]
