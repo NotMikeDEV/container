6d617cf512ee2626691faef0d14aa3ad5c5fea05
notmike-uk <notmike@notmike.co.uk>
2016-09-30 21:46:12 +0100

    Update website.lua
    should probably remove that...

--- examples/website.lua
+++ examples/website.lua
@@ -1,9 +1,6 @@
 #!/usr/sbin/container
 require("php")
 
-request_IP("66.85.79.6", {proxyarp='eth0'})
-request_IP("2001:470:3922::1:42")
-
 config_files['/etc/Caddyfile'] = [[
 http://c.notmike.uk {
 	fastcgi / /var/run/php5-fpm.sock php
