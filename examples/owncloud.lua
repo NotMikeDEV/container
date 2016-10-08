692dd864999c8a05c8c05ae43e67bf776fecb450
Mike Jones <notmike@notmike.co.uk>
2016-10-08 19:42:45 +0100

    remove ;

--- examples/owncloud.lua
+++ examples/owncloud.lua
@@ -7,6 +7,6 @@ require("templates/owncloud")
 request_IP("10.0.0.2", {nat=true})
 request_IP("2001:470:3922::1:1627", {nat=true})
 
-mysql:Database{database='owncloud'}:Grant{user='owncloud',password='owncloud'};
+mysql:Database{database='owncloud'}:Grant{user='owncloud',password='owncloud'}
 
 local TestSite = caddy:AddWebsite(owncloud:Instance{hostname='owncloud.offsite.notmike.uk'})
