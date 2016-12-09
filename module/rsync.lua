ae8eda5db430664ed174be94634a18d925d4c328
notmike-uk <notmike@notmike.co.uk>
2016-12-09 02:51:49 +0000

    Add RSync Server. Client not done yet, not sure what I want the synchign behaviour to look like.

---
+++ module/rsync.lua
@@ -0,0 +1,95 @@
+---Rsync Service.
+--@module rsync
+rsync={
+	servers={},
+	clients={},
+}
+
+---RSync Server
+--@field port integer TCP Port server is configured to listen on.
+--@table rsyncserver
+
+---RSync Server Directory.
+--@field path string Name of directory. Do not include / in the name.
+--@field localpath string Local Path to map to.
+--@table rsyncdir
+
+---RSync User.
+--@field username string
+--@field password string
+--@table rsyncuser
+
+---Add a new RSync Server.
+--@param port integer
+--@return rsyncserver
+--@usage local WebSite = caddy:AddWebsite{hostname='hostname', root='/path/to/docroot'}
+function rsync:AddServer(port)
+--	if rsync.servers[port] then return rsync.servers[port] end
+	local rsyncserver = {port=port, dirs={}}
+	---Add Directory to RSync server.
+	--@param rsyncdir
+	--@returns rsyncdir
+	function rsyncserver:AddDir(rsyncdir)
+		if not rsyncdir.users then rsyncdir.users = {} end
+		---Add User to RSync directory.
+		--@param rsyncuser
+		--@returns rsyncuser
+		function rsyncdir:AddUser(rsyncuser)
+			self.users[rsyncuser.username] = rsyncuser
+		end
+		self.dirs[rsyncdir.path] = rsyncdir
+		return rsyncdir
+	end
+	rsync.servers[port] = rsyncserver
+	return rsyncserver
+end
+
+function install_container()
+	print("Installing Rsync.")
+	install_package("rsync")
+	return 0
+end
+
+function apply_config()
+	mkdir("/etc/rsync")
+	for _,server in pairs(rsync.servers) do
+		debug_print('apply_config', "RSync Server " .. server.port)
+		local config = ""
+		config = config .. "pid file=/run/rsync." .. server.port .. ".pid\n"
+		config = config .. "lock file=/run/rsync." .. server.port .. ".lock\n"
+		config = config .. "log file=/tmp/rsync." .. server.port .. ".log\n"
+		config = config .. "port=" .. server.port .. "\n\n"
+		for _, dir in pairs(server.dirs) do
+			config = config .. "[" .. dir.path .. "]\n"
+			config = config .. "path=" .. dir.localpath .. "\n"
+			if dir.comment then config = config .. "comment=" .. dir.comment .. "\n" end
+			if dir.readonly then config = config .. "readonly=true\n" else config = config .. "readonly=false\n" end
+			config = config .. "timeout=60\n"
+			local userlist = ""
+			local passwordlist = ""
+			for _, user in pairs(dir.users) do
+				if (userlist:len() > 0) then userlist = userlist .. "," end
+				userlist = userlist .. user.username
+				passwordlist = passwordlist .. user.username .. ":" .. user.password .. "\n"
+			end
+			if (userlist:len() > 0) then
+				config = config .. "auth users=" .. userlist .. "\n"
+				config = config .. "secrets file=/etc/rsync/server." .. server.port .. ".pwd\n"
+				write_file('/etc/rsync/server.' .. server.port .. '.pwd', passwordlist)
+				exec('chmod 0600 /etc/rsync/server.' .. server.port .. '.pwd')
+			end
+		end
+		write_file('/etc/rsync/server.' .. server.port .. '.conf', config)
+	end
+	return 0
+end
+
+function background()
+	for _,server in pairs(rsync.servers) do
+		for _, dir in pairs(server.dirs) do
+			exec("mkdir -p " .. dir.localpath .. " && chmod 0777 " .. dir.localpath)
+		end
+		exec('rsync --daemon --config /etc/rsync/server.' .. server.port .. '.conf  &')
+	end
+	return 0
+end
