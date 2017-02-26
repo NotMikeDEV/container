3c2e86f7ab36cfbcea031ef1129e60e3a07632ab
notmike-uk <notmike@notmike.co.uk>
2017-02-26 22:31:16 +0000

    Fix wordpress test case

--- tests/wordpress.lua.test.sh
+++ tests/wordpress.lua.test.sh
@@ -8,7 +8,7 @@ while [ $END -gt $(date +"%s") ]; do
 	wget -T 2 -o /dev/stdout -O /dev/null "http://$IPv4:8002/readme.html" && wget -T 2 -o /dev/stdout -O /dev/null "http://[$IPv6]:8002/readme.html"
 	RET=$?
 	echo $RET
-	if [ $RET == 0 ]; then ( ../examples/wordpress.lua stop; exit 0 ) ; fi #200 OK.
+	if [ $RET == 0 ]; then ../examples/wordpress.lua stop; exit 0; fi #200 OK.
 	sleep 0.3
 done
 echo "Tests failed."
