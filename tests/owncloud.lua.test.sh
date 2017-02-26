7490cef60acfb962a61b09873b9bcc926394bf64
notmike-uk <notmike@notmike.co.uk>
2017-02-26 22:11:52 +0000

    Fix owncloud test case

--- tests/owncloud.lua.test.sh
+++ tests/owncloud.lua.test.sh
@@ -3,12 +3,12 @@
 . ./autoip.sh owncloud
 
 RET=1
-let END=$(date +"%s")+20
+let END=$(date +"%s")+120
 while [ $END -gt $(date +"%s") ]; do
 	wget -T 2 -o /dev/stdout -O /dev/null "http://[$IPv6]:8001/index.php" && wget -T 2 -o /dev/stdout -O /dev/null "http://$IPv4:8001/index.php"
 	RET=$?
 	echo $RET
-	if [ $RET == 0 ]; then ( ../examples/owncloud.lua stop; exit 0 ) ; fi #200 OK.
+	if [ $RET == 0 ]; then ../examples/owncloud.lua stop; exit 0; fi #200 OK.
 	sleep 0.3
 done
 echo "Tests failed."
